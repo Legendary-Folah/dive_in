c68926547a0708ef3054597e528fb7f879dea9fd
FolaDev <folarinfadeyi@gmail.com>
2025-03-20 10:15:10 +0100

    few changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:dive_in_app/home_page.dart';
+import 'package:dive_in_app/learn_home.dart';
 import 'package:dive_in_app/logic/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
