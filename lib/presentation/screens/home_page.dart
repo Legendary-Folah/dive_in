2da1faf2d2d1eeef59b41fcf72b483550b5f7a4e
FolaDev <folarinfadeyi@gmail.com>
2025-03-26 08:10:43 +0100

    feat: let's get started

--- lib/presentation/screens/home_page.dart
+++ lib/presentation/screens/home_page.dart
@@ -10,6 +10,23 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            width: width,
+            padding: EdgeInsets.symmetric(
+              horizontal: MediaQuery.sizeOf(context).width * 0.02,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
