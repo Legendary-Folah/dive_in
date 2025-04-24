9462ab9371b8a0f7c4c9bfb9282b55325f34da42
FolaDev <folarinfadeyi@gmail.com>
2025-04-24 09:53:59 +0100

    feat: consuming provider

--- lib/presentation/screens/home_page.dart
+++ lib/presentation/screens/home_page.dart
@@ -11,9 +11,6 @@ class HomePage extends ConsumerStatefulWidget {
 }
 
 class _HomePageState extends ConsumerState<HomePage> {
-  // late HomePageNotifier _homePageNotifier;
-  // late HomePageData _homePageData;
-
   @override
   Widget build(BuildContext context) {
     final _homePageNotifier = ref.watch(homePageProvider.notifier);
