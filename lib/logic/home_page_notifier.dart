a88972de0b2a17b492e6ff83307f7d5551fc98be
FolaDev <folarinfadeyi@gmail.com>
2025-04-25 13:22:24 +0100

    changes

--- lib/logic/home_page_notifier.dart
+++ lib/logic/home_page_notifier.dart
@@ -1,9 +1,11 @@
+import 'package:dio/dio.dart';
 import 'package:dive_in_app/models/page_data.dart';
 import 'package:dive_in_app/services/API_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get_it/get_it.dart';
 
 class HomePageNotifier extends StateNotifier<HomePageData> {
+  final String _baseUrl = "https://pokeapi.co/api/v2/";
   final GetIt _getIt = GetIt.instance;
   late ApiService _apiService;
 
@@ -12,7 +14,18 @@ class HomePageNotifier extends StateNotifier<HomePageData> {
     _setUp();
   }
 
-  Future<void> _setUp() async {}
+  Future<void> _setUp() async {
+    loadData();
+  }
+
+  Future<void> loadData() async {
+    if (state.data == null) {
+      Response? response = await _apiService.get(
+        '$_baseUrl/pokemon?limit=20&offset=0',
+      );
+      print(response.toString());
+    } else {}
+  }
 }
 
 final homePageProvider = StateNotifierProvider<HomePageNotifier, HomePageData>((
