a00365b245f12935a42ea8800aa73a67bd645c8d
FolaDev <folarinfadeyi@gmail.com>
2025-04-25 07:39:35 +0100

    initializing

--- lib/logic/home_page_notifier.dart
+++ lib/logic/home_page_notifier.dart
@@ -1,12 +1,16 @@
 import 'package:dive_in_app/models/page_data.dart';
+import 'package:dive_in_app/services/API_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get_it/get_it.dart';
 
 class HomePageNotifier extends StateNotifier<HomePageData> {
-  HomePageNotifier() : super(HomePageData.initial());
-}
+  final GetIt _getIt = GetIt.instance;
+  late ApiService _apiService;
 
-final homePageProvider = StateNotifierProvider<HomePageNotifier, HomePageData>((
-  ref,
-) {
-  return HomePageNotifier();
-});
+  HomePageNotifier(super._state) {
+    _apiService = _getIt.get<ApiService>();
+    _setUp();
+  }
+
+  Future<void> _setUp() async {}
+}
