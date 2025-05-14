89d8cb931e30137dd7d9c8c2512c8ca49cf74989
FolaDev <folarinfadeyi@gmail.com>
2025-05-14 16:26:37 +0100

    added headers to the get request

--- lib/services/API_service.dart
+++ lib/services/API_service.dart
@@ -6,7 +6,10 @@ class ApiService {
   final _dio = Dio();
   Future<Response?> get(String url) async {
     try {
-      Response response = await _dio.get(url);
+      Response response = await _dio.get(
+        url,
+        options: Options(headers: {'Content-Type': 'application/json'}),
+      );
       print(response.toString());
       return response;
     } catch (e) {
